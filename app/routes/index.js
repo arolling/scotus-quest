a567f56694a17eb1baa397cef63dbf6aa40715d0
Abigail Rolling <arolling@gmail.com>
2016-04-01 15:53:55 -0700

    preserve uniqueness of tags

--- app/routes/index.js
+++ app/routes/index.js
@@ -14,21 +14,26 @@ export default Ember.Route.extend({
       delete params.tags;
       console.log(tagArray);
       console.log(params);
-
       var newQuestion = this.store.createRecord('query', params);
-
+      var model = this.currentModel;
       for(var i=0; i < tagArray.length; i++){
         var tagString = tagArray[i];
         var tagParams = {
           words: tagString
         };
-        // this.store.queryRecord('tag', { filter: { words: tagString } }).then(function(foundTag) {
-        //   console.log(foundTag);
+        var alreadyTagged = false;
+        model.tags.forEach(function(currentTag){
+          if(currentTag.get('words') === tagString){
+            newQuestion.get('tags').addObject(currentTag);
+            alreadyTagged = true;
+          }
+        });
+        if(!alreadyTagged){
           var newTag = this.store.createRecord('tag', tagParams);
           console.log(newTag);
           newTag.save();
           newQuestion.get('tags').addObject(newTag);
-        //});
+        }
       }
       newQuestion.save();
       this.transitionTo('index');
