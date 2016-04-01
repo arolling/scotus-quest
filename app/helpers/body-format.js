c679b48f9b895880e1f3afe20e26a3b634c1260b
Abigail Rolling <arolling@gmail.com>
2016-04-01 09:53:05 -0700

    preserve linebreaks

---
+++ app/helpers/body-format.js
@@ -0,0 +1,13 @@
+import Ember from 'ember';
+
+export function bodyFormat(params/*, hash*/) {
+
+  var body = Ember.Handlebars.Utils.escapeExpression(params[0]);
+  body = body.replace(/\n\r?/g, '<br>');
+  var formatted = new Ember.Handlebars.SafeString(body);
+  return formatted;
+}
+
+export default Ember.Helper.helper(bodyFormat);
+
+//solution found at https://gist.github.com/lagartoflojo/6090175, in the replies.
---
+++ tests/unit/helpers/body-format-test.js
@@ -0,0 +1,10 @@
+import { bodyFormat } from 'scotus-quest/helpers/body-format';
+import { module, test } from 'qunit';
+
+module('Unit | Helper | body format');
+
+// Replace this with your real tests.
+test('it works', function(assert) {
+  let result = bodyFormat([42]);
+  assert.ok(result);
+});
